1fed7115dab199a6ccc98612d755c561cc9813ca
SatyamNamdevv <ssnamdev2117@gmail.com>
2025-06-02 12:47:44 +0000

    prefix and postfix

---
+++ JsBasics/prepost.js
@@ -0,0 +1,16 @@
+// pre fix and post fix operator
+
+//let x = 3; //3 raha
+//const y = x++; // x 3 rha but fir 4 hogya
+
+//console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+let x = 10;
+console.log(++x);
+console.log(x);
