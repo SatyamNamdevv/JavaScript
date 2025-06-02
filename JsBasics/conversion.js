d9511856a7dab5b0bba2d304df372a3c2c1e7ce0
SatyamNamdevv <ssnamdev2117@gmail.com>
2025-06-02 12:30:52 +0000

    conversion rule

---
+++ JsBasics/conversion.js
@@ -0,0 +1,5 @@
+/* conversion is use to convert from one to another data type , and we have to take care of string to number and 
+number to string data type conversion */
+console.log("1"+2); //contactnating string
+console.log(1+4+"2"); // adding only number and then concatnating string
+
