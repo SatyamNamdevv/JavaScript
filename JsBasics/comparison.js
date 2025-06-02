46b0752b41f3ff4cb32340fd52b8f7245b937f1a
SatyamNamdevv <ssnamdev2117@gmail.com>
2025-06-02 14:15:46 +0000

    comparision operator

--- JsBasics/comparison.js
+++ JsBasics/comparison.js
@@ -1,1 +1,15 @@
 // comparison operator
+
+console.log(null>0);
+console.log(null ==0);
+console.log(null>=0);
+console.log(null===0);
+
+console.log(2>4);
+
+console.log("a"=="a");
+console.log(1===1);
+console.log(undefined==0);
+console.log(undefined>0);
+
+//Note we have to ignore this types of comparision this is not clean code
