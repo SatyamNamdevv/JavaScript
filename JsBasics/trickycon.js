4ace482776975653679564b307e318fcfad25354
SatyamNamdevv <ssnamdev2117@gmail.com>
2025-06-02 12:39:30 +0000

    tricky conversion

---
+++ JsBasics/trickycon.js
@@ -0,0 +1,14 @@
+//here is some tricky conversion to be noticed
+
+console.log(true) // works clearly
+console.log(+true); // Not a good way to write code
+//console.log(true+); will give error
+console.log(+""); // not a good way to write code 
+//-----------------------------------
+// NOTHER example not to be used
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2 ;
+ // Not a standard way of coding
+console.log(num1);
+// Note : Code must be readable must be understandable
