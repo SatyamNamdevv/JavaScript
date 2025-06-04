9ac7d681373b1cc7e7c736ed4bc1af17ccebdd0e
SatyamNamdevv <ssnamdev2117@gmail.com>
2025-06-04 08:41:11 +0000

    Function2

---
+++ JsMedium/function2.js
@@ -0,0 +1,16 @@
+// SHpoing price exmple no idea or parameter
+function calculate(...num1){
+    return num1
+}
+console.log(calculate(233,444,555,566))
+// this is called rest operator as well as spread operator depends on usecase
+const user = {
+    username: "Sayyyam",
+    price:199
+
+}
+function handobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+handobject(user);
+// de care of typechecckkkk
