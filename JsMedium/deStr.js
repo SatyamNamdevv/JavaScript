dbf22b81829225624ab66d0e1c400f7aea80b7b3
SatyamNamdevv <ssnamdev2117@gmail.com>
2025-06-04 07:55:16 +0000

    de-Structure and API

---
+++ JsMedium/deStr.js
@@ -0,0 +1,16 @@
+// Desctructure of a function is much needed
+
+const course ={
+    coursename: "React",
+    price: "6767",
+    courseInstructor: "Ishyam"
+}
+const {courseInstructor : instructor}= course
+console.log(courseInstructor)
+// API Concept
+// Apna kaam doosre ke sar
+//Jason is object {}. json example
+// {
+//     "name": "satyam",
+//     "coursenamee": "js",
+// }
