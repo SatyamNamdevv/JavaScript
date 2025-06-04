048694333276d09fa72c2d7461a22f7b8a61431b
SatyamNamdevv <ssnamdev2117@gmail.com>
2025-06-04 08:23:57 +0000

    Function part 1

---
+++ JsMedium/function.js
@@ -0,0 +1,36 @@
+// Function in depth
+
+function main(){
+    console.log("hello")
+    console.log("hello")
+    console.log("hello")
+    console.log("hello")
+    console.log("hello")
+    console.log("hello")
+    console.log("hello")
+}
+main()
+
+
+// function twoSum(num1, num2) {
+//     console.log(num1 + num2)
+    
+// }twoSum(4,5)// these are arguments// 
+
+// optimise method
+function twoSum(num1, num2) {
+  
+    let result = num1+num2
+    return  result
+    
+}
+let sum = twoSum(6,8)
+console.log(sum)
+
+function loginMessage(username) {
+    return `${username} just logged in`
+    
+}
+//loginMessage("Satyam")// but this will not print cos print to kro bs value di hai isme
+
+console.log(loginMessage("Satyam"))
