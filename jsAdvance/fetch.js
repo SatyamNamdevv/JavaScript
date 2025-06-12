34e6204c4ea67cceffb235da4fa3dfdbe86ce683
SatyamNamdevv <ssnamdev2117@gmail.com>
2025-06-12 10:43:47 +0000

    fetch in js

---
+++ jsAdvance/fetch.js
@@ -0,0 +1,20 @@
+//Fetch JS
+
+async function getData() {
+  const url = "https://example.org/products.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+const response = await fetch("https://example.org/post", {
+  method: "POST",
+  // …
+});
